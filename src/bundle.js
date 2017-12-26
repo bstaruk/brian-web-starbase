926cf316803431b50ceee12c31376fc0e752d832
Brian Staruk <brian@staruk.me>
2017-12-26 10:33:52 -0500

    theme buildout

--- src/bundle.js
+++ src/bundle.js
@@ -1,9 +1,6 @@
-// components js
-import Button from './components/button/Button';
-
 // promise & fetch polyfills
-require('es6-promise').polyfill();
-require('whatwg-fetch');
+// require('es6-promise').polyfill();
+// require('whatwg-fetch');
 
 // baseline app styles
 require('./app/fonts/fonts.css');
@@ -11,9 +8,3 @@ require('./app/app.css');
 
 // components styles
 require.context('./components/', true, /\.css$/);
-
-// button component
-const buttons = document.getElementsByClassName('button');
-for (let i = 0; i < buttons.length; i++) {
-  new Button({ el: buttons[i] });
-}
