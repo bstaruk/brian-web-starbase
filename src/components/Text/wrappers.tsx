c7464f372f5b8448799e217d249a4ede2cd20301
Brian Staruk <brian@staruk.net>
2022-12-21 16:28:07 -0500

    fonts buildout

--- src/components/Text/wrappers.tsx
+++ src/components/Text/wrappers.tsx
@@ -8,13 +8,33 @@ export const Wrapper = styled.div<TextProps>`
   ${p =>
     p.variant === 'body' &&
     `
-    font-size: 17px;
+    font-size: 18px;
+    line-height: 1.4;
+    letter-spacing: 0.02em;
+    font-family: ${p.theme.fonts.sans};
+    font-weight: 400;
+
+    @media (min-width: 420px) {
+      font-size: 19px;
+    }
   `}
 
   /* h1 */
   ${p =>
     p.variant === 'h1' &&
     `
-    font-size: 48px;
+    font-size: 32px;
+    line-height: 1.2;
+    letter-spacing: 0.02em;
+    font-family: ${p.theme.fonts.serif};
+    font-weight: 700;
+
+    @media (min-width: 420px) {
+      font-size: 46px;
+    }
+
+    @media (min-width: 760px) {
+      font-size: 64px;
+    }
   `}
 `;
