347cfefbc110c990546f411b08d664eed1b4d151
Brian Staruk <brian@staruk.net>
2022-12-21 16:13:43 -0500

    stripping-out redux, reducing boilerplate

--- src/data/app/selectors.ts
+++
@@ -1,8 +0,0 @@
-import { createSelector } from '@reduxjs/toolkit';
-
-import type { RootState } from 'data/store';
-import { initialState, sliceKey } from './slice';
-
-const selectDomain = (state: RootState) => state[sliceKey] || initialState;
-
-export const selectCount = createSelector([selectDomain], state => state.count);
--- src/testUtils.tsx
+++ src/testUtils.tsx
@@ -1,26 +1,22 @@
 import React, { FC, ReactElement } from 'react';
 import { render, RenderOptions } from '@testing-library/react';
 import { Global, ThemeProvider } from '@emotion/react';
-import { Provider } from 'react-redux';
 import { HelmetProvider } from 'react-helmet-async';
 import { BrowserRouter } from 'react-router-dom';
 
-import { store } from 'data/store';
 import styles from 'style/globalStyle';
 import theme from 'style/theme';
 
 const AllTheProviders: FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
-    <Provider {...{ store }}>
-      <HelmetProvider>
-        <BrowserRouter>
-          <ThemeProvider {...{ theme }}>
-            {children}
-            <Global {...{ styles }} />
-          </ThemeProvider>
-        </BrowserRouter>
-      </HelmetProvider>
-    </Provider>
+    <HelmetProvider>
+      <BrowserRouter>
+        <ThemeProvider {...{ theme }}>
+          {children}
+          <Global {...{ styles }} />
+        </ThemeProvider>
+      </BrowserRouter>
+    </HelmetProvider>
   );
 };
 
