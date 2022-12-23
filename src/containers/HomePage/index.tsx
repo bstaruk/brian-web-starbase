d96e8c0d52b34ea18d970b33a4df016663039954
Brian Staruk <brian@staruk.net>
2022-12-22 21:28:15 -0500

    removing home title

--- src/containers/HomePage/index.tsx
+++ src/containers/HomePage/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Helmet } from 'react-helmet-async';
 
 import Text from 'components/Text';
 import SocialLinks from 'components/SocialLinks';
@@ -7,44 +6,38 @@ import { ReactComponent as DownIcon } from './assets/down.svg';
 import { Wrapper, PageWrapper, SectionWrapper, DownIconWrapper } from './wrappers';
 
 const HomePage = () => (
-  <>
-    <Helmet>
-      <title>Home</title>
-    </Helmet>
+  <Wrapper>
+    <PageWrapper>
+      <SectionWrapper>
+        <Text as="h1" variant="h1">
+          brian.staruk.net
+        </Text>
 
-    <Wrapper>
-      <PageWrapper>
-        <SectionWrapper>
-          <Text as="h1" variant="h1">
-            brian.staruk.net
-          </Text>
+        <Text as="p">
+          <>
+            I{"'"}m a husband, animal lover, gamer and homebrewer from Boston, MA. More relevantly,
+            I{"'"}m a front-end web developer with a passion for user and dev experience. Crafting
+            polished, accessible interfaces with React and implementing them with an airtight CI/CD
+            flow is my specialty.
+          </>
+        </Text>
+      </SectionWrapper>
 
-          <Text as="p">
-            <>
-              I{"'"}m a husband, animal lover, gamer and homebrewer from Boston, MA. More
-              relevantly, I{"'"}m a front-end web developer with a passion for user and dev
-              experience. Crafting polished, accessible interfaces with React and implementing them
-              with an airtight CI/CD flow is my specialty.
-            </>
-          </Text>
-        </SectionWrapper>
+      <DownIconWrapper aria-hidden="true">
+        <DownIcon />
+      </DownIconWrapper>
+    </PageWrapper>
 
-        <DownIconWrapper aria-hidden="true">
-          <DownIcon />
-        </DownIconWrapper>
-      </PageWrapper>
+    <PageWrapper>
+      <SectionWrapper>
+        <Text as="h2" variant="h2">
+          <>let{"'"}s connect</>
+        </Text>
 
-      <PageWrapper>
-        <SectionWrapper>
-          <Text as="h2" variant="h2">
-            <>let{"'"}s connect</>
-          </Text>
-
-          <SocialLinks />
-        </SectionWrapper>
-      </PageWrapper>
-    </Wrapper>
-  </>
+        <SocialLinks />
+      </SectionWrapper>
+    </PageWrapper>
+  </Wrapper>
 );
 
 export default HomePage;
