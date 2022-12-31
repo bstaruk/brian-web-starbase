a346c84b492147c0660aae2150ee3426ffb380d2
Brian Staruk <brian@staruk.net>
2022-12-30 20:37:08 -0500

    a modicum of motion

--- src/components/SocialLinks/index.tsx
+++ src/components/SocialLinks/index.tsx
@@ -5,9 +5,24 @@ import links from './links';
 import { ListWrapper, ListItemWrapper, LinkWrapper, LinkLabelWrapper } from './wrappers';
 
 const SocialLinks = () => (
-  <ListWrapper>
+  <ListWrapper initial="offscreen" whileInView="onscreen" viewport={{ once: true, amount: 0.8 }}>
     {links.map(({ href, icon, label, title }, index) => (
-      <ListItemWrapper key={index}>
+      <ListItemWrapper
+        key={index}
+        variants={{
+          offscreen: {
+            x: -50 - index * 50,
+          },
+          onscreen: {
+            x: 0,
+            transition: {
+              type: 'spring',
+              bounce: 0.4,
+              duration: 0.8,
+            },
+          },
+        }}
+      >
         <LinkWrapper target="_blank" {...{ href, title }}>
           {icon}
           <LinkLabelWrapper>
