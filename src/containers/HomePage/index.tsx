61016fcbdfafe9714325c075706bb2008b420b99
Brian Staruk <brian@staruk.net>
2023-07-10 11:44:21 -0400

    animation adjustments

--- src/containers/HomePage/index.tsx
+++ src/containers/HomePage/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 
 import Text from 'components/Text';
 import SocialLinks from 'components/SocialLinks';
@@ -9,20 +10,34 @@ const HomePage = () => (
   <Wrapper>
     <PageWrapper>
       <SectionWrapper>
-        <TitleWrapper as="h1" variant="h1">
-          <>
-            brian<span>.</span>staruk<span>.net</span>
-          </>
-        </TitleWrapper>
+        <motion.div
+          initial={{ x: -32, opacity: 0 }}
+          whileInView={{ x: 0, opacity: 1 }}
+          transition={{ delay: 0, duration: 0.3 }}
+          viewport={{ amount: 0.1, once: false }}
+        >
+          <TitleWrapper as="h1" variant="h1">
+            <>
+              brian<span>.</span>staruk<span>.net</span>
+            </>
+          </TitleWrapper>
+        </motion.div>
 
-        <Text as="p">
-          <>
-            I{"'"}m a husband, animal lover, gamer and homebrewer from Boston, MA. I{"'"}m also a
-            front-end web developer with a passion for user and dev experience. Crafting polished,
-            accessible interfaces with React and implementing them with an airtight CI/CD flow is my
-            specialty.
-          </>
-        </Text>
+        <motion.div
+          initial={{ x: -32, opacity: 0 }}
+          whileInView={{ x: 0, opacity: 1 }}
+          transition={{ delay: 0.15, duration: 0.3 }}
+          viewport={{ amount: 0.1, once: false }}
+        >
+          <Text as="p">
+            <>
+              I{"'"}m a husband, animal lover, gamer and homebrewer from Boston, MA. I{"'"}m also a
+              front-end web developer with a passion for user and dev experience. Crafting polished,
+              accessible interfaces with React and implementing them with an airtight CI/CD flow is
+              my specialty.
+            </>
+          </Text>
+        </motion.div>
       </SectionWrapper>
 
       <DownIconWrapper aria-hidden="true">
@@ -32,9 +47,16 @@ const HomePage = () => (
 
     <PageWrapper>
       <SectionWrapper>
-        <Text as="h2" variant="h2">
-          <>let{"'"}s connect</>
-        </Text>
+        <motion.div
+          initial={{ x: -32, opacity: 0 }}
+          whileInView={{ x: 0, opacity: 1 }}
+          transition={{ delay: 0, duration: 0.3 }}
+          viewport={{ amount: 0.5, once: false }}
+        >
+          <Text as="h2" variant="h2">
+            <>let{"'"}s connect</>
+          </Text>
+        </motion.div>
 
         <SocialLinks />
       </SectionWrapper>
